d0b6f64ea9f4542483385a378baadeebb5463efa
zengze <zengze@smart>
2018-01-08 11:00:52 +0800

    调整api

--- init-react/src/constants/Api.js
+++ init-react/src/constants/Api.js
@@ -1,10 +1,10 @@
 import TokenUtil from '../common/TokenUtil';
-import { message } from 'antd'
+import { Toast } from 'antd-mobile'
 export default class Api {
     //static URL= 'http://192.168.100.37:9090/happy_module_userinfo/';
     // static MENU_URL = 'http://localhost:8080/MENU.json';
-    static MENU_URL = '/MENU.json';
-    static URL = 'http://localhost:8090/';
+    static MENU_URL = './MENU.json';
+    static URL= 'http://192.168.10.199:8090/';
     static dealy = (flag = false, ms = 3000) => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
@@ -38,7 +38,7 @@ export default class Api {
             .then(res => res.json())
             .then(json => {
                 if (json.code == -1) {
-                    message.info('登录超时！请重新登录', 2)
+                    Toast.info('登录超时！请重新登录', 1)
                     window.location.href = '/login'
                 } else {
                     return json
